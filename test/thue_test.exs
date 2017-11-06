662872da7114e186df70e0480df527eca2d84909
Walter Dal Mut <walter.dalmut@gmail.com>
2017-11-06 12:04:22 +0100

    Added simple test case

--- test/thue_test.exs
+++ test/thue_test.exs
@@ -9,5 +9,7 @@ defmodule ThueTest do
     assert Thue.get(4) == "0110"
     assert Thue.get(5) == "01101"
     assert Thue.get(6) == "011010"
+    assert Thue.get(63) == "011010011001011010010110011010011001011001101001011010011001011"
+    assert Thue.get(64) == "0110100110010110100101100110100110010110011010010110100110010110"
   end
 end
